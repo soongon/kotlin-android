2f95e90c4c0114896a45979ce7784c662f7e9899
soongon <soongon@gmail.com>
2019-06-04 22:40:01 +0900

    network

--- app/src/main/java/com/soongon/nationinfo/service/network/Retrofit.kt
+++ app/src/main/java/com/soongon/nationinfo/service/network/Retrofit.kt
@@ -1,25 +1,30 @@
-package com.soongon.marvelgallery.data.network.provider
+package com.soongon.nationinfo.service.network
 
 import com.google.gson.Gson
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val retrofit by lazy { makeRetrofit() }
 
 private fun makeRetrofit(): Retrofit = Retrofit.Builder()
-    .baseUrl("https://gateway.marvel.com/v1/public/")
+    .baseUrl("https://jsonplaceholder.typicode.com/")
     .client(makeHttpClient())
     .addConverterFactory(GsonConverterFactory.create(Gson()))
-    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
     .build()
 
 private fun makeHttpClient() = OkHttpClient.Builder()
     .connectTimeout(60, TimeUnit.SECONDS)
     .readTimeout(60, TimeUnit.SECONDS)
     .addInterceptor(makeHeadersInterceptor())
-    .addInterceptor(makeAddSecurityQueryInterceptor())
-    .addInterceptor(makeLoggingInterceptor())
     .build()
+
+fun makeHeadersInterceptor() = Interceptor { chain ->
+    chain.proceed(chain.request().newBuilder()
+        .addHeader("Accept", "application/json")
+        .addHeader("Accept-Language", "en")
+        .addHeader("Content-Type", "application/json")
+        .build())
+}
